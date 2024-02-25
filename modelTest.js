bcec89e12b18f11409b35d11ff6e9fb2d283ee4a
rutuja177 <rutuja.dukhande7@gmail.com>
2024-02-24 20:06:50 -0600

    initial model test

---
+++ modelTest.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const faceapi = require('face-api.js');
+
+const { Canvas, Image, ImageData } = require('canvas');
+const canvas = require('canvas');
+
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+
+async function detectFaces(){
+    // Load face detection models
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(__dirname + "/models");
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(__dirname + "/models");
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(__dirname + "/models");
+
+  console.log(Object.keys(faceapi.nets))
+}
+detectFaces()
